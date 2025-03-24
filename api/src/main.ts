ce3a5a403459e6986596da7fd209e22af17ac63e
LucasSachs1 <Lucas_Sachs@outlook.com>
2025-03-24 17:34:27 -0300

    Fix > Problemas com CORS na comunicação Backend - Frontend

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors({
+		origin: ['http://localhost:3000']
+	});
+
 	await app.listen(process.env.PORT ?? 8000);
 }
 
